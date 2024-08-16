dbc53f88d887db2fb2b36cf168acef52c1fe613f
BAMBARA Clement <98878922+Chapelie@users.noreply.github.com>
2024-08-16 01:08:58 +0000

    Python projet

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for char in str:
+        if ord(char) >= 97 and ord(char) <= 122:
+            print(chr(ord(char) - 32), end="")
+        else:
+            print(char, end="")
+    print()
